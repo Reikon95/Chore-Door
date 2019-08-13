db198702ce8bd60c2c52ddaa00b377633524c43e
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-08-13 13:19:05 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let doorImage1 = document.getElementById('door1');
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+
+
+doorImage1.onclick = () => {
+  doorImage1.src = botDoorPath;
+}
+
